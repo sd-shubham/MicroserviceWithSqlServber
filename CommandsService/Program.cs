b66ccee30fa0a237e04a5171ae520892a36c51ef
Shubham Dubey <Shubham.Dubey@birchford.com>
2021-10-02 22:53:55 +0530

    changes

--- CommandsService/Program.cs
+++ CommandsService/Program.cs
@@ -7,27 +7,20 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace PlatformService
+namespace CommandsService
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-
             CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-            .ConfigureLogging(logging =>
-        {
-            logging.ClearProviders();
-            logging.AddConsole();
-        })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
                 });
     }
-
 }
